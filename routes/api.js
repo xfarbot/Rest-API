e7f7de2bbe8d6ad118f399ecd0884ae1ff623e22
viko-api <vikodwik@gmail.com>
2021-07-17 21:27:06 +0700

    “.”

--- routes/api.js
+++ routes/api.js
@@ -175,7 +175,7 @@ Akhir Pesan Error
 
 router.use(favicon(__path + "/views/favicon.ico"));
 
-const listkey = ["Fxc7", "Vikodk"];
+const listkey = ["Fxc7", "Vikodk", "rxking"];
 
 router.post("/apikey", async (req, res, next) => {
   const key = req.query.key;
