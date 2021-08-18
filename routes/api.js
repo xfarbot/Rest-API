de09fb35b88ce3d5f497678d44c9e384ec239f24
Viko Dwi Kurniawan <85740085+vikodk67@users.noreply.github.com>
2021-08-18 18:23:06 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -251,6 +251,29 @@ router.get('/hack/tlpn', async(req, res, next) => {
 }
 })
 
+router.get('/random/informasi', async (req, res, next) => {
+        var Apikey = req.query.apikey
+            
+	if(!Apikey) return res.json(loghandler.notparam)
+	if(listkey.includes(Apikey)){
+
+       fetch(encodeURI(`http://studiovdk.herokuapp.com/api/ingfoku.php`))
+        .then(response => response.json())
+        .then(data => {
+        var result = data.result;
+             res.json({
+                 creator : `${creator}`,
+                 result
+             })
+         })
+         .catch(e => {
+         	res.json(loghandler.error)
+})
+} else {
+res.json(loghandler.invalidKey)
+}
+})
+
 router.get('/hack/sms', async(req, res, next) => {
   const apikey = req.query.apikey;
   const query = req.query.query;
